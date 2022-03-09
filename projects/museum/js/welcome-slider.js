6e3c9aa976187b14bba01f9bb4e877f066323ee8
egorokunevich <84414222+egorokunevich@users.noreply.github.com>
2022-03-09 17:26:53 +0300

    Welcome slider beta

---
+++ projects/museum/js/welcome-slider.js
@@ -0,0 +1,52 @@
+// let slider = document.querySelector("div.slider");
+// let sliderItems = document.querySelector("div.welcome__slides");
+let slides = document.querySelectorAll("div.welcome__slide");
+let prev = document.querySelector("div.slider__arrow_left");
+let next = document.querySelector("div.slider__arrow_right");
+let sliderDot = document.querySelector(".slider__dot");
+
+let sliderIndex = 2;
+let position = `translateX(-${(sliderIndex * 1000)}px)`;
+
+prev.onclick = function prevSlide() {
+  if (sliderIndex == 0) {
+    sliderIndex = 4;
+  }
+  else {
+    sliderIndex--;
+  }
+  changePosition();
+  dotActive();
+}
+next.onclick = function nextSlide() {
+  if (sliderIndex == 4) {
+    sliderIndex = 0;
+  }
+  else {
+    sliderIndex++;
+  }
+  changePosition();
+  dotActive();
+}
+
+function changePosition() {
+  position = `translateX(-${(sliderIndex * 1000)}px)`;
+  for (let i = 0 ; i < slides.length; i++) {
+      slides[i].style.transform = position;
+  }
+}
+
+function dotActive() {
+  for (let i = 0 ; i < sliderDot.length; i++) {
+    console.log("index = ", sliderIndex);
+    console.log("dot = ", sliderDot[i]);
+    if (i == sliderIndex) {
+      sliderDot[i].classList.add("slider__dot_active");
+    }
+    else {
+      sliderDot[i].classList.remove("slider__dot_active");
+    }
+    console.log("index = ", sliderIndex);
+    console.log("dot = ", sliderDot[i]);
+}
+}
