e26327ea53292acded804ae7d3aeb278dfc97ccb
egorokunevich <84414222+egorokunevich@users.noreply.github.com>
2022-03-09 20:08:38 +0300

    Add files via upload

--- projects/museum/js/welcome-slider.js
+++ projects/museum/js/welcome-slider.js
@@ -1,55 +1,53 @@
 let slider = document.querySelector("div.slider");
-// let sliderItems = document.querySelector("div.welcome__slides");
 let slides = document.querySelectorAll("div.welcome__slide");
 let prev = document.querySelector("div.slider__arrow_left");
 let next = document.querySelector("div.slider__arrow_right");
-let sliderDot = document.querySelector(".slider__dot");
+let sliderDot = document.querySelectorAll(".slider__dot");
 
-let sliderIndex = 2;
-let position = `translateX(-${(sliderIndex * 1000)}px)`;
+let sliderIndex = 0; // Current slide index
+let position = `translateX(-${sliderIndex * 1000}px)`; // Slides' offset
 let sliderWidth = slider.clientWidth;
 console.log(sliderWidth);
 
+// Previous slide
 prev.onclick = function prevSlide() {
   if (sliderIndex == 0) {
     sliderIndex = 4;
-  }
-  else {
+  } else {
     sliderIndex--;
   }
   changePosition();
   dotActive();
-}
+};
+
+// Next slide
 next.onclick = function nextSlide() {
   if (sliderIndex == 4) {
     sliderIndex = 0;
-  }
-  else {
+  } else {
     sliderIndex++;
   }
   changePosition();
   dotActive();
-}
+};
 
+// Moves slides
 function changePosition() {
   sliderWidth = slider.clientWidth;
-  position = `translateX(-${(sliderIndex * sliderWidth)}px)`;
-  for (let i = 0 ; i < slides.length; i++) {
-      slides[i].style.transform = position;
+  position = `translateX(-${sliderIndex * sliderWidth}px)`;
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = position;
   }
 }
 
+// Moves active dot
 function dotActive() {
-  for (let i = 0 ; i < sliderDot.length; i++) {
-    console.log("index = ", sliderIndex);
-    console.log("dot = ", sliderDot[i]);
+  for (let i = 0; i < sliderDot.length; i++) {
     if (i == sliderIndex) {
       sliderDot[i].classList.add("slider__dot_active");
     }
     else {
       sliderDot[i].classList.remove("slider__dot_active");
     }
-    console.log("index = ", sliderIndex);
-    console.log("dot = ", sliderDot[i]);
-}
+  }
 }
